e5e49be2da8391e5d4b86b358ea5bc2dc2e8baa7
DevArthuro <caorrego38@soy.sena.edu.co>
2024-05-26 17:40:06 -0500

    feat: add toast notifications and create handle create user

--- frontend/src/handlers/users.ts
+++ frontend/src/handlers/users.ts
@@ -1,12 +1,25 @@
 import { axiosInstance as request } from "@/utils";
-import { UserData } from "@/types";
+import { UserData, UserDataExcludeId } from "@/types";
 
 export const getAllUsers = async () => {
   try {
     const response = await request.get("/users");
     const data: UserData[] = await response.data;
-    return data.reverse();
+    return data;
   } catch (error) {
     console.log("Error fetching data", error);
+  }
+};
+
+export const createUser = async (
+  userData: UserDataExcludeId
+): Promise<{ data?: UserData; status: boolean }> => {
+  try {
+    const response = await request.post("/users", userData);
+    const data: UserData = await response.data;
+    return { status: true, data };
+  } catch (error) {
+    console.log("Error fetching data", error);
+    return { status: false };
   }
 };
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import Layout from "@/common/layout";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import "react-toastify/ReactToastify.min.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
